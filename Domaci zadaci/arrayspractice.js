cf8860ce2d94d86ffcd2c55e7064a93eb3be225a
maja-n <maja.ninkovic.vg@gmail.com>
2021-12-15 16:39:34 +0100

    added second assignment

--- Domaci zadaci/arrayspractice.js
+++ Domaci zadaci/arrayspractice.js
@@ -1,6 +1,16 @@
 'use strict'
 
+// 1. zadatak
 let foodproducers=["Bambi", "Neoplanta", "Podravka"];
 
-foodproducers.shift("Barilla")
-foodproducers.push("Meggle", "Yuhor")
+foodproducers.unshift("Barilla");
+foodproducers.push("Meggle", "Yuhor");
+
+// 2. zadatak
+console.log(foodproducers)
+
+for (let i=0; i < foodproducers.lenght; i++) {
+    console.log(foodproducers[i]);   
+}
+
+// 3. zadatak
